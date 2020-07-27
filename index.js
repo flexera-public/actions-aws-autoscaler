07fbf9b37f57041791fb19a17dc9e39ef03797b0
Mohammad Forutan <mforutan@users.noreply.github.com>
2020-07-27 13:51:20 +1000

    initial version

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const AWS = require('aws-sdk');
+const child_process = require("child_process");
 
 
-// most @actions toolkit packages have async methods
+const GROUP_NAME = core.getInput('groupName', {
+  required: true
+});
+
+const TIMEOUT = core.getInput('timeoutMinutes', {
+  required: false
+});
+
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    var params = {
+      AutoScalingGroupName: GROUP_NAME,
+      DesiredCapacity: 1
+    };
 
-    core.setOutput('time', new Date().toTimeString());
+    var autoscaling = new AWS.AutoScaling();
+    autoscaling.setDesiredCapacity(params, function (err, data) {
+      if (err) throw err;
+      else core.info("Successfully set the desired capacity.");
+    });
+
+    params = {
+      AutoScalingGroupNames: [
+        GROUP_NAME
+      ]
+    };
+
+    core.info("Wait for runner to be ready.");
+
+    var timeout_ms = TIMEOUT * 60 * 1000;
+    var startTime = Date.now();
+    while ((Date.now() - startTime) < timeout_ms) {
+      var data = await autoscaling.describeAutoScalingGroups(params).promise();
+      if (data.AutoScalingGroups[0].Instances[0] != null && data.AutoScalingGroups[0].Instances[0].LifecycleState === "InService") {
+        core.info("Runner is ready.")
+        break;
+      }
+      core.info("Not in service yet!")
+      child_process.execSync("sleep 2");
+    }
   } catch (error) {
+    core.error(error);
     core.setFailed(error.message);
   }
 }
